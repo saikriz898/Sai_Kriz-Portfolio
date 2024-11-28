7b4b4a3d45701834888a1e55790bfc9cc3ddb602
SAI KRISHNAN.S <saikrishnan.s2024cse@sece.ac.in>
2024-11-28 16:23:34 +0000

    refactor navigation functions to improve side navigation behavior

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.transition = "0.5s ease-in-out";
 }
